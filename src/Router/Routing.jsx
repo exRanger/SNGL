8432791576441caa1daee39d395e461c6403aacf
Ilgiz <lmnasxzz@gmail.com>
2021-04-06 09:35:12 +0300

    Add files via upload

---
+++ src/Router/Routing.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import {Main, About, Settings, NotFound} from '../pages'
+import {links} from '../constants/links'
+import { Navbar, Loader } from '../components'
+export const Routing = () => { 
+    return (
+         (
+            <Router>
+                <Navbar/>
+                
+                <Switch>
+                        <Route exact  path={links.main}>                   
+                            <Main/>
+                        </Route>
+                        <Route path={links.about}>                        
+                            <About/>
+                        </Route>
+                        <Route path={links.settings}>   
+                            <Settings/>
+                        </Route>
+                        <NotFound/>
+                </Switch>
+            </Router>
+        )
+    )
+}

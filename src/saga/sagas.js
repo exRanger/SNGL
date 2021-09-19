72d288453a11926c73a05f2480005fa6fc174e55
Ilgiz <lmnasxzz@gmail.com>
2021-09-20 00:04:02 +0300

    Update sagas.js

--- src/saga/sagas.js
+++ src/saga/sagas.js
@@ -4,7 +4,6 @@ import { LOAD_DATA, putData, PUT_DATA } from '../store/mock'
 function fetchData() {
     return fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
     .then(response => response.json())
-  
 }
  
 function* workerLoadData(){

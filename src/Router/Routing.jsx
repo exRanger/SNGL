ce8c3e8dd64e6557381847b0f2e6c943b1cdfa76
Ilgiz <lmnasxzz@gmail.com>
2021-09-25 06:28:44 +0300

    Update Routing.jsx

--- src/Router/Routing.jsx
+++ src/Router/Routing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 import {Main, About, Settings, NotFound} from '../pages'
 import {links} from '../constants/links'
 import { Navbar } from '../components'

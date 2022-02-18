f279cfbbf9dfac1fbf9fa5ea01ce35298de776ee
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-02-19 00:04:11 +0300

    Update sagas.js

--- src/saga/sagas.js
+++ src/saga/sagas.js
@@ -3,7 +3,7 @@ import { LOAD_DATA, putData, PUT_DATA } from '../store/mock'
  
 function fetchData() {
     return fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-    .then(response => response.json())
+      .then(response => response.json())
 }
  
 function* workerLoadData(){

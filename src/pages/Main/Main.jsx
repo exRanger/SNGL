ef69cda0fff140d9b80107a9600c120367bb2a4b
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-06-07 22:15:54 +0300

    Update Main.jsx

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import { Loader, Animator } from '../../components'
-import { loadData } from '../../store/mock'
+import {Loader, Animator} from '../../components'
+import {loadData} from '../../store/mock'
 import './Main.css'
+
 export const Main= () => {
+    
     const fetched = useSelector(state => state.fetchReducer.fetched)
     const {loading}= useSelector(state => state.loaderReducer)
     const dispatch = useDispatch()
-    if(loading){
+    
+    if (loading) {
         return <Loader/>
     }
     
-    const handleClick  =  (e)  => {
+    const handleClick = (e)  => {
         dispatch(loadData())
-         console.log(fetched)
     }
-       return (
+  
+    return (
         <Animator>
              <div className="main">
              <h1>Main Page</h1>
-              <button onClick={handleClick}>upload async</button> 
+              <button onClick={handleClick}>Make a fetch request</button> 
                <div>
                    {fetched.map((elem)=>{return <div key={elem.id}>{elem.title}</div>})}
                 </div>   

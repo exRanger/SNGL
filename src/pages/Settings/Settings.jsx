1d29c3a7a7babf9579d0f9c9ab6071f3243e8a1c
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-06-07 22:13:58 +0300

    Update Settings.jsx

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
-import { Animator } from '../../components'
+import {Animator} from '../../components'
 import {Loader} from '../../components/Loader'
+
 export const Settings = () => {
     
     const {loading} = useSelector(state => state.loaderReducer)
     
-    if(loading){
+    if (loading) {
         return <Loader/>       
     }
     return (
         <Animator>
             <div className="settings">
-             <div className="row">
-                <div className="block block-left"></div>
-                <div className="block block-left"></div>
-                <div className="block block-right"></div>
-             </div>
-             <div className="row">
-                <div className="block block-left"></div>
-                <div className="block block-right"></div>
+                 <div className="row">
+                    <div className="block block-left"></div>
+                    <div className="block block-left"></div>
+                    <div className="block block-right"></div>
+                 </div>
+                 <div className="row">
+                    <div className="block block-left"></div>
+                    <div className="block block-right"></div>
+                </div>
             </div>
-        </div>
         </Animator>
     )
 }

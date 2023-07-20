e02b4fac661d37b0f251a78521f1d51837eea43c
Zagirov Il'giz <lmnasxzz@gmail.com>
2023-07-20 22:21:53 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import "./Profile.css"
-
 import {useSelector} from 'react-redux'
 
 export const Profile = () => {
-
     const {profile} = useSelector((state) => state.profileReducer)
-    
     return (
         <div className='profile'>
            {  profile ? <h1>{profile.name}</h1> : <h1>no-data</h1>}
